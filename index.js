a1e378b05e3c88c4c14c45a9c5660f54750e73c4
chaimstanton1@gmail.com <38401049+ChZeSt@users.noreply.github.com>
2018-11-14 22:09:34 +0000

    reformat code

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ var playerSpeedY = 0;
 //only allow jumping if the player is not already jumping
 var jumping = false;
 
-function preload(){
+function preload() {
   spikeImg = loadImage("Smilee.png")
 }
 
 function setup() {
-   
+
   createCanvas(300, 300);
-  background(0, 0, 255);        
-  
+  background(0, 0, 255);
+
   mainCharacter = createSprite(1, 1);
   mainCharacter.scale = 0.2;
   mainCharacter.addImage(spikeImg);
@@ -33,37 +33,37 @@ function setup() {
   mainCharacter.position.y = groundY - 20;
   console.log(mainCharacter.width);
 
- }
+}
 
 function draw() {
-  mainCharacterBottomPosition = mainCharacter.position.y - 20 
+  mainCharacterBottomPosition = mainCharacter.position.y - 20
 
   userInput() // to see if key is pressed
   userInputSprite(mainCharacter)
 
-  background(0, 0, 255); 
-  
+  background(0, 0, 255);
+
   //draw the ground
   stroke(255);
   line(0, groundY, width, groundY);
 
   //move the player
   playerY += playerSpeedY;
-  playerGroundHightCollisionStopper(); 
+  playerGroundHightCollisionStopper();
 
   //draw the player rectangle
   rect(playerX, playerY, playerWidth, playerHeight);
 
-  drawSprites();   
+  drawSprites();
 }
 
-function playerGroundHightCollisionStopper(){
-    //is the player colliding with the ground?
+function playerGroundHightCollisionStopper() {
+  //is the player colliding with the ground?
   if (playerY + playerHeight >= groundY) {
-  //snap the player's bottom to the ground's position
+    //snap the player's bottom to the ground's position
     playerY = groundY - playerHeight;
 
-  //stop the player falling
+    //stop the player falling
     playerSpeedY = 0;
 
     //allow jumping again
@@ -72,18 +72,18 @@ function playerGroundHightCollisionStopper(){
   // //player is not colliding with the ground
   else {
     //gravity accelerates the movement speed
-    playerSpeedY ++;
-  } 
+    playerSpeedY++;
+  }
 }
 
-function userInput(){ // This is called in the draw funciton and makes it smoothly move left and right
-  if (keyIsDown(39)){
-    if ((playerX + playerWidth) < width){
+function userInput() { // This is called in the draw funciton and makes it smoothly move left and right
+  if (keyIsDown(39)) {
+    if ((playerX + playerWidth) < width) {
       playerX += 4
-      }
     }
-  else if(keyIsDown(37)){
-    if (playerX > 1){
+  }
+  else if (keyIsDown(37)) {
+    if (playerX > 1) {
       playerX -= 4
     }
   }
@@ -91,21 +91,21 @@ function userInput(){ // This is called in the draw funciton and makes it smooth
   if (!jumping && keyCode === 32) {
     //going up
     playerSpeedY = -15;
-    
+
     //disallow jumping while already jumping
     jumping = true;
   }
-  keyCode = 0; 
+  keyCode = 0;
 }
 
-function userInputSprite(sprite){ // This is called in the draw funciton and makes it smoothly move left and right z
-  if (keyIsDown(39)){
-    if ((sprite.position.x + sprite.width) < width){
+function userInputSprite(sprite) { // This is called in the draw funciton and makes it smoothly move left and right z
+  if (keyIsDown(39)) {
+    if ((sprite.position.x + sprite.width) < width) {
       sprite.position.x += 4
-      }
     }
-  else if(keyIsDown(37)){
-    if (sprite.position.x > 1){
+  }
+  else if (keyIsDown(37)) {
+    if (sprite.position.x > 1) {
       sprite.position.x -= 4
     }
   }
@@ -117,7 +117,7 @@ function userInputSprite(sprite){ // This is called in the draw funciton and mak
     //disallow jumping while already jumping
     jumping = true;
   }
-  keyCode = 0; 
+  keyCode = 0;
 }
 
 function ignore() {
